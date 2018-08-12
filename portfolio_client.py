c42a8149d75bf931d367a098319cef78cf6b13e6
Meghan E <erickson.meghan@gmail.com>
2018-08-12 14:02:23 -0600

    first through and through working while integrated with s3

---
+++ portfolio_client.py
@@ -0,0 +1,60 @@
+import json
+import logging
+import botocore
+from io import BytesIO
+
+BUCKET = "expansellc-datalake"
+KEY = "portfolio/portfolio.csv"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+class PortfolioClient:
+    def __init__(self, s3):
+        self.s3 = s3
+        self.exists = True
+        try:
+            self.s3.head_bucket(Bucket=BUCKET)
+        except botocore.exceptions.ClientError as e:
+            error_code = int(e.response['Error']['Code'])
+            if error_code == 404:
+                logging.error("PortfolioClient: "
+                              "The {} bucket could not be found. ".format(BUCKET))
+                self.exists = False
+
+    def send_to_bucket(self, df):
+        if self.exists:
+            logging.info("PortfolioClient : attempting to send portfolio to "
+                         "destination {} {}".format(BUCKET, KEY))
+            df_buffer = BytesIO()
+            df_buffer.write(b"timestamp,total\n")
+            df_buffer.write(bytes(df.to_csv(), 'utf-8'))
+            df_buffer.seek(0)
+            try:
+                self.s3.upload_fileobj(df_buffer, BUCKET, KEY)
+            except botocore.exceptions.ClientError as e:
+                msg = e.response.get('Error', {}).get('Message', 'Could Not Retrieve Error Message')
+                logging.error("PortfolioClient BotocoreClientException sending portfolio "
+                              "to destination {} {}: {}"
+                              .format(BUCKET, KEY, msg))
+            except Exception as e:
+                logging.error("PortfolioClient Exception end portfolio to"
+                              "destination {} {}: {}"
+                              .format(BUCKET, KEY, e))
+        else:
+            logging.error("PortfolioClient : failed to send portfolio to"
+                          "destination {} {}".format(BUCKET, KEY))
+
+    def get_portfolio_from_bucket(self):
+        """
+            Gets the existing portfolio from the bucket.
+            :return stream of bytes
+        """
+        if self.exists:
+            logging.info("PortfolioClient : attempting to get portfolio from "
+                         "src {} {}".format(BUCKET, KEY))
+            result = self.s3.get_object(Bucket=BUCKET, Key=KEY)
+            return result['Body'].read()
+        else:
+            logging.error("PortfolioClient : failed to get portfolio from"
+                          " src expansellc-datalake/portfolio/portfolio.csv.")
--- service.py
+++ service.py
@@ -1,9 +1,19 @@
 import pandas as pd
+import logging
+import boto3
+from portfolio_client import PortfolioClient
 
 
 class Service:
+    '''
+    When this is ported to the serverless project,
+    the PortfolioClient will be imported from the shared py utils package
+    '''
     def __init__(self):
-        self.file = 'data/portfolio.csv'
+        logging.info("Instantiating Service. First time load of portfolio from s3 . . .")
+        s3 = boto3.client('s3', region_name='us-west-2')
+        client = PortfolioClient(s3)
+        self.portfolio = client.get_portfolio_from_bucket()
 
     def get_portfolio(self):
-        return self.file
+        return self.portfolio
